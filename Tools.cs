7a06459bd79d8bacf551bb837e3d481e3726010a
vlad-cv <me@vladcv.dev>
2025-12-11 00:28:25 -0600

    Add Tools.WorkingWithTypes to display type hierarchy
    Replaced "Hello, World!" in Program.cs with a call to Tools.WorkingWithTypes(). Introduced the Tools class with a method that prints the type and base types of an integer array, demonstrating type inspection in C#.

---
+++ Tools.cs
@@ -0,0 +1,19 @@
+﻿namespace _01_Csharp_Big_Picture;
+
+internal class Tools
+{
+    public static void WorkingWithTypes()
+    {
+        var numbers = new int[] { 1, 2, 3, 4, 5 };
+        var typeInfo = numbers.GetType();
+
+
+        do
+        {
+            Console.WriteLine($"Type: {typeInfo.FullName}");
+            typeInfo = typeInfo.BaseType;
+        }
+        while (typeInfo != null);
+    }
+}
+
